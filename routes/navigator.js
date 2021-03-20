4ac7864ea8bce1d44a17d38f8d7daf19952dfe66
Milica Andric <milicaandric087@gmail.com>
2021-03-20 13:18:28 -0500

    added screens to stack

--- routes/navigator.js
+++ routes/navigator.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../screens/login';
+//import Signup from '../screens/signup';
+import Password from '../screens/password';
 import { AppRegistry } from 'react-native';
 import {name as appName} from '../app.json';
 AppRegistry.registerComponent(appName, () => App);
@@ -12,6 +14,7 @@ export default function Navigator(){
     <NavigationContainer>
       <AppStack.Navigator screenOptions={{ headerShown: false}} >
         <AppStack.Screen name="Login" component={Login}/>
+        <AppStack.Screen name="Password" component={Password}/>
       </AppStack.Navigator>
     </NavigationContainer>
     );
