0cdecc223156ddd8168cc9ab42f1325094c79c72
Milica Andric <milicaandric087@gmail.com>
2021-03-29 22:19:19 -0500

    added add and remove bank screens to navigator

--- routes/navigator.js
+++ routes/navigator.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../screens/login';
-//import Signup from '../screens/signup';
 import Password from '../screens/password';
 import Signup_Merchant from '../screens/signup_merchant';
 import Signup_Customer from '../screens/signup_customer';
 import User_Dash from '../screens/user_dash';
 import Merchant_Dash from '../screens/merchant_dash';
+import Add_Bank from '../screens/add_bank';
+import Remove_Bank from '../screens/remove_bank';
+
 import { AppRegistry } from 'react-native';
 import {name as appName} from '../app.json';
 AppRegistry.registerComponent(appName, () => App);
@@ -23,7 +25,9 @@ export default function Navigator(){
         <AppStack.Screen name='Signup_Merchant' component={Signup_Merchant}/>
         <AppStack.Screen name='Signup_Customer' component={Signup_Customer}/>
         <AppStack.Screen name="Password" component={Password}/>
-        
+        <AppStack.Screen name="Add_Bank" component={Add_Bank}/>
+        <AppStack.Screen name="Remove_Bank" component={Remove_Bank}/>
+
       </AppStack.Navigator>
     </NavigationContainer>
     );
