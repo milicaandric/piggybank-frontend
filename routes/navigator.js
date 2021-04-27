8ddf4515b12362887c1c8a3ff9555f2d24025bb3
Milica Andric <milicaandric087@gmail.com>
2021-04-27 12:12:05 -0500

    add about nav

--- routes/navigator.js
+++ routes/navigator.js
@@ -28,7 +28,6 @@ export default function Navigator(){
     <NavigationContainer>
       <AppStack.Navigator screenOptions={{ headerShown: false}} >
         <AppStack.Screen name="Login" component={Login}/>
-        
         <AppStack.Screen name="User_Dash" component={User_Dash} options={{gestureEnabled: false}}/>
         <AppStack.Screen name="Merchant_Dash" component={Merchant_Dash} options={{gestureEnabled: false}}/>
         <AppStack.Screen name='Signup_Merchant' component={Signup_Merchant} options={{gestureEnabled: false}}/>
